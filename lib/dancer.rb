6580ee93391e924f41138fb0b53cd2f59a9078d3
Keanna Salter <keannasalter91@gmail.com>
2020-04-03 05:48:42 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++
@@ -1,8 +0,0 @@
-class Dancer
-  include Dance       #=> "include" - These are instance methods
-  extend MetaDancing  #=> "extend"  - These are class methods
-  attr_accessor :name
-  def initialize(name)
-    @name = name
-  end
-end
